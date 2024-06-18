58104adea9d09f170b6e24f04f2e14b9207e0410
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-06-18 14:57:11 +0700

    refactor: remove unused command

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,41 +1,6 @@
 -- plugin: noice.nvim
 
 local ops = {
-  -- lsp = {
-  --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --   override = {
-  --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --     ["vim.lsp.util.stylize_markdown"] = true,
-  --     ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-  --   },
-  --
-  --   progress = {
-  --     enabled = false,
-  --     format = "lsp_progress",
-  --     format_done = "lsp_progress_done",
-  --     throttle = 1000 / 30, -- frequency to update lsp progress message
-  --     view = "mini",
-  --   },
-  -- },
-  -- -- you can enable a preset for easier configuration
-  -- presets = {
-  --   bottom_search = true, -- use a classic bottom cmdline for search
-  --   command_palette = true, -- position the cmdline and popupmenu together
-  --   long_message_to_split = true, -- long messages will be sent to a split
-  --   inc_rename = false, -- enables an input dialog for inc-rename.nvim
-  --   lsp_doc_border = false, -- add a border to hover docs and signature help
-  -- },
-  --
-  -- routes = {
-  --   {
-  --     filter = {
-  --       event = "msg_show",
-  --       kind = "",
-  --       find = "written",
-  --     },
-  --     opts = { skip = true },
-  --   },
-  -- },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
@@ -84,19 +49,6 @@ local ops = {
       },
       opts = { skip = true },
     },
-
-    -- {
-    --   filter = {
-    --     event = "msg_show",
-    --     any = {
-    --       { find = "%d+L, %d+B" },
-    --       { find = "; after #%d+" },
-    --       { find = "; before #%d+" },
-    --       { find = "fewer lines" },
-    --     },
-    --   },
-    --   view = "mini",
-    -- },
   },
 }
 
