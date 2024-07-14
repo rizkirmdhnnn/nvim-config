05f4cd49a3c8c57647f2b2434d7898d49b496231
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-07-14 10:56:12 +0700

    chore: change keymap for lazygit

--- lua/plugins/lazy-git.lua
+++ lua/plugins/lazy-git.lua
@@ -13,10 +13,8 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
 }
